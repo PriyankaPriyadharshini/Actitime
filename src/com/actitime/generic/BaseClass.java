cddb8bc6866be2b4edaf095c5ea736782b833804
Priyanka <priyadharshini20594@gmail.com>
2022-12-21 22:01:43 +0530

    New actitime

---
+++ src/com/actitime/generic/BaseClass.java
@@ -0,0 +1,40 @@
+package com.actitime.generic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import com.actitime.pom.EnterTimeTrackPage;
+
+public class BaseClass {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+	public static WebDriver driver;
+	@BeforeClass
+	public void openBrowser() {
+		Reporter.log("openBrowser",true);
+	}
+	@AfterClass
+	public void closeBrowser() {
+		Reporter.log("closeBrowser",true);
+	}
+	@BeforeMethod
+	public void login() {
+		Reporter.log("login",true);
+		driver.get("https://demo.actitime.com/login.do");
+		
+	}
+	@AfterMethod
+	public void logout() {
+		/*Reporter.log("logout",true);
+		EnterTimeTrackPage e=new EnterTimeTrackPage(driver);
+		e.setLogout();*/
+		
+		
+	}
+}
---
+++ src/com/actitime/testscript/CustomerModule.java
@@ -0,0 +1,17 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.actitime.generic.BaseClass;
+import com.actitime.pom.EnterTimeTrackPage;
+
+public class CustomerModule extends BaseClass {
+	@Test
+	public void createCustomer() {
+		Reporter.log("createCustomer",true);
+		//EnterTimeTrackPage e=new EnterTimeTrackPage(driver);
+		
+	}
+
+}
